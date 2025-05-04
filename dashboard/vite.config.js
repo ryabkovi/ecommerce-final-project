6febce610101c23357e2e1c856a5c0c6a1e57eaa
ryabkovi <roye212@gmail.com>
2025-05-04 07:14:35 +0300

    fix: added spa support

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -11,4 +12,10 @@ export default defineConfig({
   build: {
     outDir: "dist",
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  appType: "spa",
 });
