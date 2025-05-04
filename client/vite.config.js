611520d1f79664224e5b2c7cc83854ff7e8436c0
ryabkovi <roye212@gmail.com>
2025-05-04 05:52:29 +0300

    edit vite.config.js side client and dashboard

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,5 +6,6 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: parseInt(process.env.PORT) || 5173,
+    allowedHosts: ["ecommerce-final-project-client.onrender.com"],
   },
 });
