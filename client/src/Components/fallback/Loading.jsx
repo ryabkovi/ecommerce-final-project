0d982081dddea953b642c905cec0dc4c0758fd36
ryabkovi <roye212@gmail.com>
2025-05-06 22:42:11 +0300

    🔥 Improve product loading with auto-refresh until fetch success

--- client/src/Components/fallback/Loading.jsx
+++ client/src/Components/fallback/Loading.jsx
@@ -1,15 +1,3 @@
-// function Loading() {
-//   return (
-//     <div className="flex justify-center items-center h-screen">
-//       <div
-//         className="spinner-border animate-spin inline-block w-8 h-8 border-4 border-t-4 border-gray-500 rounded-full"
-//         role="status"
-//       ></div>
-//     </div>
-//   );
-// }
-
-// export default Loading;
 function Loading() {
   return (
     <div className="flex justify-center items-center h-[300px]">
--- client/src/Components/ui/ProductList.jsx
+++ client/src/Components/ui/ProductList.jsx
@@ -1,37 +1,10 @@
-// import React, { useContext } from "react";
-// import ProductCard from "./ProductCard";
-// import { GlobalContext } from "../../context/GlobalContext";
-// import "./ProductList.css";
-
-// function ProductList() {
-//   const { products = [] } = useContext(GlobalContext);
-
-//   return (
-//     <div className="product-list">
-//       {products.length > 0 ? (
-//         products
-//           .slice(0, 4)
-//           .map((product) => <ProductCard key={product._id} product={product} />)
-//       ) : (
-//         <p>No products available.</p>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default ProductList;
 import React, { useContext } from "react";
 import ProductCard from "./ProductCard";
 import { GlobalContext } from "../../context/GlobalContext";
-import Loading from "../fallback/Loading";
 import "./ProductList.css";
 
 function ProductList() {
-  const { products = [], isLoading } = useContext(GlobalContext);
-
-  if (isLoading) {
-    return <Loading />;
-  }
+  const { products = [] } = useContext(GlobalContext);
 
   return (
     <div className="product-list">
